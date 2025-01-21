a8bdebf24707a0e9c759c14dd143a288488d53e5
dpfka23 <yerama777@naver.com>
2025-01-21 17:53:40 +0900

    update component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import './App.scss';
+import Header from './components/Header';
+import Banner from './components/Banner';
+import FAQ from './components/FAQ';
 
 const App = () => {
   return (
-    <div>
-      
+    <div className='app'>
+      <Header />
+      <Banner />
+      <FAQ />
     </div>
   );
 };
